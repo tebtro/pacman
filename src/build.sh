821dac2bdc31e28e05ac66ed948b767169879e18
tebtro <robert.ortner.aut@gmail.com>
2019-12-15 10:12:24 +0100

    Added SDL2 libs and include files to project Update linux build script.

--- src/build.sh
+++ src/build.sh
@@ -3,7 +3,20 @@
 mkdir -p ../run_tree
 pushd ../run_tree
 
-clang --debug -Wno-null-dereference -Wno-writable-strings -Wno-undefined-internal ../src/sdl2_pacman.cpp -o linux_pacman -g -I/usr/include/SDL2 -DBUILD_INTERNAL=1 -DBUILD_SLOW=1 -DBUILD_LINUX=1 -D_REENTRANT -lSDL2 -lm -fno-exceptions
+
+common_compiler_flags="-Wno-write-strings -Wno-unused-variable -Wno-sign-compare -Wno-null-dereference -Wno-missing-braces -fno-rtti -fno-exceptions -Wno-undefined-internal -DBUILD_INTERNAL=1 -DBUILD_SLOW=1 -DBUILD_LINUX=1"
+
+# build 64-bit
+clang --debug $common_compiler_flags ../src/sdl2_pacman.cpp -o linux_pacman.x86_64 -g `../libs/sdl2_x64/bin/sdl2-config --cflags --libs` -Wl,-rpath,'$ORIGIN/x86_64' -lm
+
+# build 32-bit
+#clang --debug $common_compiler_flags ../src/sdl2_pacman.cpp -o linux_pacman.x86 -g `../libs/sdl2_x32/bin/sdl2-config --cflags --libs` -Wl,-rpath,'$ORIGIN/x86' -lm
+
+# copy SDL2 into output directory
+mkdir -p x86_64
+cp ../libs/sdl2_x64/lib/libSDL2-2.0.so.0 x86_64/
+mkdir -p x86
+cp ../libs/sdl2_x32/lib/libSDL2-2.0.so.0 x86/
 
 
 popd
